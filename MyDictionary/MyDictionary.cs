4a3845cc3874295f66e1dd12d14e9bcf551d1c08
fatih demirören <afatih9@windowslive.com>
2021-01-25 21:42:10 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey , TValue>           
+    {
+        Tkey[] tkeys;
+        TValue[] tvalues;
+
+        public MyDictionary()
+        {
+            tkeys = new Tkey[0];
+            tvalues = new TValue[0];
+        }
+
+        public void Add(Tkey tkey, TValue tvalue)
+        {
+            Tkey[] temptkeys = tkeys;
+            tkeys = new Tkey[tkeys.Length + 1];
+            TValue[] tempvalues = tvalues;
+            tvalues = new TValue[tvalues.Length + 1];
+
+            for (int i = 0; i < temptkeys.Length; i++)
+            {
+                tkeys[i] = temptkeys[i];
+            }
+
+            tkeys[tkeys.Length - 1] = tkey;
+
+            for (int i = 0; i < tempvalues.Length; i++)
+            {
+                tvalues[i] = tempvalues[i];
+            }
+            tvalues[tvalues.Length - 1] = tvalue;                       
+        }
+
+        public int Length
+        {
+            get { return tkeys.Length; }
+        }
+
+        public Tkey[] Tkeys
+        {
+            get { return tkeys; }
+        }
+
+        public TValue[] TValues
+        {
+            get { return tvalues; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> standing = new MyDictionary<string, int>();
+            standing.Add("Real Madrid", 15);
+            standing.Add("Chelsea", 13);
+            standing.Add("Basel", 4);
+            standing.Add("D.Kiev", 1);
+
+            Console.WriteLine("Takım Sayısı :" + standing.Length);
+
+            Console.WriteLine("1. {0}, Puan :{1}",standing.Tkeys[0],standing.TValues[0]);
+            Console.WriteLine("2. {0}, Puan :{1}", standing.Tkeys[1], standing.TValues[1]);
+            Console.WriteLine("3. {0}, Puan :{1}", standing.Tkeys[2], standing.TValues[2]);
+            Console.WriteLine("4. {0}, Puan :{1}", standing.Tkeys[3], standing.TValues[3]);
+
+        }
+    }
+}
